f689c21a5bbccd623d27c6c0f3c87d960164b238
Karoly Richard Hollo <tevesek3@gmail.com>
2022-08-25 17:20:05 +0200

    Game.tscn: - draw players sanctuaries, --- - add master and student sprites as scenes - make screen bigger (108x768)

--- grid/GridVisualizer.gd
+++ grid/GridVisualizer.gd
@@ -3,22 +3,29 @@ extends Node2D
 const BACKGROUND_COLOR = Color.burlywood
 const LINE_COLOR = Color(0, 0, 0, 0.2)
 const LINE_WIDTH = 2
-
+const RED_PLAYER_SENTRY_COLOR = Color(204, 0, 0, 0.1)
+const BLUE_PLAYER_SENTRY_COLOR = Color( 0.254902, 0.411765, 0.882353, 0.2 )
 onready var grid = $"%Grid"
 
 func _ready():
-	#modulate = Color.burlywood
 	pass
 
 func _draw():
 	#var window_size = OS.get_window_size()
 	draw_background()
+	draw_player_sanctuary(Vector2(2, 0), RED_PLAYER_SENTRY_COLOR)
+	draw_player_sanctuary(Vector2(2, 4), BLUE_PLAYER_SENTRY_COLOR)
 	draw_columns()
 	draw_rows()
 
 func draw_background():
 	var grid_size = grid.grid_size.x * grid.tile_size.x
 	draw_rect(Rect2(grid.position, Vector2(grid_size, grid_size)), BACKGROUND_COLOR, true)
+	
+func draw_player_sanctuary(grid_position: Vector2, color: Color):
+	var position = grid.map_to_world(grid_position)
+	var size = grid.tile_size.x
+	draw_rect(Rect2(position, Vector2(size, size)), color)
 
 func draw_columns():
 	for x in range(grid.grid_size.x + 1):
