2e624b032cfec6b01fefa731fbbb0d27aec6dc2a
Karoly Richard Hollo <tevesek3@gmail.com>
2022-09-02 01:01:37 +0200

    Manage that only one pawn could be selected at one time

--- student/Student.gd
+++ student/Student.gd
@@ -10,8 +10,16 @@ func _init():
 	game = Global.ref["Game"]
 
 func _on_StudentArea_input_event(viewport, event, shape_idx):
-	Utils.handle_left_mouse_button_selection(event, self)
-	
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		Global.selected_pawn = self
-		game.manage_card_and_pawn_selection()
+		if Global.is_any_pawn_selected:
+			if !self.selected:	#this pawn is not the selected one but there is another one which is selected
+				return
+				
+			Global.selected_pawn = null
+			Global.is_any_pawn_selected = false
+			game.manage_card_and_pawn_selection()
+		else:
+			Global.selected_pawn = self
+			Global.is_any_pawn_selected = true
+			game.manage_card_and_pawn_selection()
+	Utils.handle_left_mouse_button_selection(event, self)
