5a3be0da75726fcc58c3e3fe8008b7899e1fef22
Karoly Richard Hollo <tevesek3@gmail.com>
2022-09-02 20:02:45 +0200

    - refactor: remove duplicate function calls, extract it to a common place

--- pawn/Pawn.gd
+++ pawn/Pawn.gd
@@ -15,13 +15,12 @@ func _on_Area_input_event(viewport, event, shape_idx) -> void:
 		if Global.is_any_pawn_selected:
 			if !self.selected:	#this pawn is not the selected one but there is another one which is selected
 				return
-				
-			Utils.handle_left_mouse_button_selection(event, self)
+
 			Global.selected_pawn = null
 			Global.is_any_pawn_selected = false
-			game.handle_draw_of_moves()
 		else:
-			Utils.handle_left_mouse_button_selection(event, self)
 			Global.selected_pawn = self
 			Global.is_any_pawn_selected = true
-			game.handle_draw_of_moves()
+
+		Utils.handle_left_mouse_button_selection(event, self)
+		game.handle_draw_of_moves()
