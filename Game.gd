e659cbea52a3a3b9f7dee48c3f786a8cd86ed493
Karoly Richard Hollo <tevesek3@gmail.com>
2022-09-02 20:17:03 +0200

    - refactor on line 34, - add out of bounds check when drawing possible moves

--- Game.gd
+++ Game.gd
@@ -1,5 +1,8 @@
 class_name Game extends Node2D
 
+const BOARD_MIN_INDEX = 0
+const BOARD_MAX_INDEX = 4
+
 onready var grid = $"%Grid"
 onready var down_left_card = $"%Card1"
 onready var Move = preload("res://move/Move.tscn")
@@ -26,9 +29,9 @@ func handle_draw_of_moves():
 	if is_card_selected && is_pawn_selected:
 		var pawn_coordinate: Vector2 = grid.world_to_map(Global.selected_pawn.position)
 		draw_possible_moves(pawn_coordinate, Global.selected_card)
+		return
 		
-	else:
-		remove_moves_from_board()
+	remove_moves_from_board()
 
 func remove_moves_from_board():
 	if move_list.empty():
@@ -41,11 +44,20 @@ func remove_moves_from_board():
 
 func draw_possible_moves(pawn_base_coord: Vector2, card):
 	for move in card.moves:
+		var move_coord = pawn_base_coord + move
+		if is_coord_out_of_bounds(move_coord):
+			continue
+		
 		var move_visual = Move.instance()
 		move_list.append(move_visual)
 		move_visual.modulate = move_color
 		
-		var move_coord = pawn_base_coord + move
 		move_visual.position = grid.map_to_world(move_coord) + grid.half_tile_size
 		
 		add_child(move_visual)
+
+func is_coord_out_of_bounds(coord: Vector2) -> bool:
+	return true if is_index_out_of_bounds(coord.x) || is_index_out_of_bounds(coord.y) else false
+		
+func is_index_out_of_bounds(coord_index) -> bool:
+	return true if coord_index < BOARD_MIN_INDEX || coord_index > BOARD_MAX_INDEX else false
